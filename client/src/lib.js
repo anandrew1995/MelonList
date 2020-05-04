f835e55aff65b765427d877e6f9bf3de6b71fa86
Jeong Won An <jw3an@uwaterloo.ca>
2020-05-03 21:28:59 -0400

    Change github condition

--- client/src/lib.js
+++ client/src/lib.js
@@ -1,10 +1,8 @@
 import config from "../src/config.json";
 
 export const getServerHost = () => {
-	if (
-		process.env.NODE_ENV === "development" ||
-		config.onGithubPages === true
-	) {
+	const isOnGithub = window.location.href.includes("github.io");
+	if (process.env.NODE_ENV === "development" || isOnGithub) {
 		return config.serverHost;
 	}
 	return "";
