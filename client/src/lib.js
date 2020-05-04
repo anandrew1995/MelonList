9a627c277e7ce56f0d2d7bf55f87c3a72dd3680b
Jeong Won An <jw3an@uwaterloo.ca>
2020-05-03 21:20:48 -0400

    Added setting for github pages

--- client/src/lib.js
+++ client/src/lib.js
@@ -1,7 +1,10 @@
 import config from "../src/config.json";
 
 export const getServerHost = () => {
-	if (process.env.NODE_ENV === "development") {
+	if (
+		process.env.NODE_ENV === "development" ||
+		config.onGithubPages === true
+	) {
 		return config.serverHost;
 	}
 	return "";
